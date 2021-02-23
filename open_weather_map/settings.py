0d71102ca818e35143a48cb7739ce3071e94a435
Mhlengi Mzimela <mhlengij5@gmail.com>
2021-02-23 18:22:25 +0200

    Configured mysql DB

--- open_weather_map/settings.py
+++ open_weather_map/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'open_weather_map.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'open_weather_map',
+        'USER': 'blah',
+        'PASSWORD': 'mhlengi',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'TEST': {
+            'NAME': 'test_open_weather_map',
+        },
     }
 }
 
