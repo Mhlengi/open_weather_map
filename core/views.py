9ab0c2551cb9a795fedc88de9ad675677a9dac90
Mhlengi Mzimela <mhlengij5@gmail.com>
2021-02-23 20:27:57 +0200

    Added base.html link css

--- core/views.py
+++ core/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template_variables = {'a': 1, 'b': 2}
+    return render(request, 'core/index.html', template_variables)
